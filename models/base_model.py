842fb1cc30d91519c9377d012edd877c998368f5
tercsam <mascret.clement@gmail.com>
2024-03-06 16:55:36 -0800

    update base model file

--- models/base_model.py
+++ models/base_model.py
@@ -1,30 +1,22 @@
-#!/usr/bin/python3
-"""BaseModel class"""
 import uuid
-import datetime
+from datetime import datetime
 
 
 class BaseModel:
-    """BaseModel class"""
     def __init__(self):
-        """__init__ method"""
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.utcnow()
-        self.updated_at = datetime.datetime.utcnow()
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
-        """__str__ method"""
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        """save method"""
-        self.updated_at = datetime.datetime.utcnow()
+        self.updated_at = datetime.now()
 
     def to_dict(self):
-        """to_dict method"""
-        new_dict = self.__dict__.copy()
-        new_dict['__class__'] = self.__class__.__name__
-        new_dict['id'] = self.id
-        new_dict['created_at'] = self.created_at.isoformat()
-        new_dict['updated_at'] = self.updated_at.isoformat()
-        return new_dict
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
