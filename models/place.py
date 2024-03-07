c34cd01b94976a7d5919bc667114c4f5e4ca90da
Arweenn <arwendumont25@outlook.fr>
2024-03-07 16:48:00 +0100

    a useless 's'

--- models/place.py
+++ models/place.py
@@ -12,7 +12,7 @@ class Place(BaseModel):
     description = ""
     number_rooms = 0
     number_bathrooms = 0
-    max_guests = 0
+    max_guest = 0
     price_by_night = 0
     latitude = 0.0
     longitude = 0.0
