800c52acd14cf9cdfb4ab60fa1038a8ebb4d090e
tercsam <mascret.clement@gmail.com>
2024-03-06 16:54:12 -0800

    update file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -30,13 +30,29 @@ class FileStorage:
 
     def reload(self):
         """reload method"""
-        from models import user
-        from models import city
-        from models import state
-        from models import place
-        from models import review
-        from models import amenity
-        from models import base_model
+    import models.user as user_module
+    import models.city as city_module
+    import models.state as state_module
+    import models.place as place_module
+    import models.review as review_module
+    import models.amenity as amenity_module
+    import models.base_model as base_model_module
+
+    dict_module = {'BaseModel': base_model_module, 'User': user_module,
+                   'State': state_module, 'Place': place_module,
+                   'City': city_module, 'Amenity': amenity_module,
+                   'Review': review_module}
+
+    if os.path.exists(FileStorage.__file_path):
+        with open(FileStorage.__file_path, 'r') as f:
+            loaded_objects = json.load(f)
+            for key, value in loaded_objects.items():
+                class_name = value['__class__']
+                if class_name in dict_module:
+                    model_module = dict_module[class_name]
+                    model_class = getattr(model_module, class_name)
+                FileStorage.__objects[key] = model_class(**value)
+
 
         dict_module = {'BaseModel': base_model, 'User': user,
                        'State': state, 'Place': place,
