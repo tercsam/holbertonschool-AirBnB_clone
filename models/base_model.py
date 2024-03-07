bf80fa5c106587319f2c59e9de08d10044f610e4
tercsam <mascret.clement@gmail.com>
2024-03-06 16:21:18 -0800

    update base model file

--- models/base_model.py
+++ models/base_model.py
@@ -1,22 +1,58 @@
+#!/usr/bin/python3
+"""
+Module that define the BaseModel class.
+"""
+
 import uuid
 from datetime import datetime
+from models import storage
 
 
 class BaseModel:
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+    """
+    BaseModel class that define common attributes and methods.
+    """
+
+    def __init__(self, *args, **kwargs):
+        """
+        Initializes a new instance of the BaseModel class.
+        """
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    setattr(self, key, value)
+                if key == "created_at" or key == "updated_at":
+                    date = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, date)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
-        return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
+        """
+        Returns a string representation of the instance.
+        """
+        class_name = self.__class__.__name__
+        return f"[{class_name}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        self.updated_at = datetime.now()
+        """
+        Updates the updated_at attribute with the current date and time.
+        """
+        self.updated_at = datetime.utcnow()
+        storage.save()
 
     def to_dict(self):
-        obj_dict = self.__dict__.copy()
-        obj_dict['__class__'] = type(self).__name__
-        obj_dict['created_at'] = self.created_at.isoformat()
-        obj_dict['updated_at'] = self.updated_at.isoformat()
-        return obj_dict
+        """
+        Returns a dictionnary representation of the instance.
+        """
+        new_dict = self.__dict__.copy()
+
+        new_dict["__class__"] = self.__class__.__name__
+        new_dict['id'] = self.id
+        new_dict["created_at"] = self.created_at.isoformat()
+        new_dict["updated_at"] = self.updated_at.isoformat()
+
+        return new_dict
